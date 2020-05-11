e4492a468dbca54f072a4b6f5c2726212df2e7b2
Paula <paula.krupa@gmail.com>
2020-05-11 20:40:27 +0200

    Updates

---
+++ precache-manifest.4c150d7d8331e65b35b885c1461e169c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e3c9c30760daaa8221805ff3a4e08b",
+    "url": "/finalproject/index.html"
+  },
+  {
+    "revision": "c05d09cfd9ad3276b3cc",
+    "url": "/finalproject/static/css/main.5b640541.chunk.css"
+  },
+  {
+    "revision": "41c0619bcab2371beb95",
+    "url": "/finalproject/static/js/2.7d6874df.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/finalproject/static/js/2.7d6874df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c05d09cfd9ad3276b3cc",
+    "url": "/finalproject/static/js/main.cd5b4e9b.chunk.js"
+  },
+  {
+    "revision": "7001e452cd8d44866a0f",
+    "url": "/finalproject/static/js/runtime-main.05fd81fd.js"
+  }
+]);
