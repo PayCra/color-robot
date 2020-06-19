61515cfdc95590a0ee4cb6773837a01b4d696a30
paula <paula.krupa@gmail.com>
2020-06-19 19:06:25 +0200

    Scaled for Mac.

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ class App extends Component {
                 text: <>Complementary color scheme is intense and draws immediate attention.<br/>
                     It can be too strong and produce a clashing effect. Use with caution!</>
 
-        })
+            })
         } else if (colorScheme === "Analogous") {
             this.setState({
                 shapeColor1: "color" + color,
@@ -63,7 +63,7 @@ class App extends Component {
                 selectedColorScheme: colorScheme,
                 text: <>Split complementary is a popular and elegant color scheme for rich but harmonious design.<br/>
                     Hint: choose one main color and use the other two as accents.</>
-                    })
+            })
         } else if (colorScheme === "Triadic") {
             this.setState({
                 shapeColor1: "color" + color,
@@ -76,7 +76,7 @@ class App extends Component {
                 selectedColorScheme: colorScheme,
                 text: <>Triadic color scheme is for the bold.<br/>
                     Hint: choose one main color and use the other two as accents.</>
-                    })
+            })
         } else if (colorScheme === "Tetradic") {
             this.setState({
                 shapeColor1: "color" + color,
@@ -89,7 +89,7 @@ class App extends Component {
                 selectedColorScheme: colorScheme,
                 text: <>Tetradic color scheme will make your design look rich, yet cohesive.<br/>
                     Hint: choose one main color and use the other three as accents.</>
-                    })
+            })
         } else if (colorScheme === "Square") {
             this.setState({
                 shapeColor1: "color" + color,
@@ -102,7 +102,7 @@ class App extends Component {
                 selectedColorScheme: colorScheme,
                 text: <>Square color scheme looks lively and abundant.<br/>
                     Hint: choose one main color and use the other three as accents.</>
-                    })
+            })
         }
     };
 
@@ -111,51 +111,80 @@ class App extends Component {
             <div>
                 <div className="Title">Color Scheme Robot</div>
                 <div className="Container">
-                <div>
-                    <span className={"Label " + (this.state.selectedColorScheme === "Complementary" ? "Selected" : "")}>Complementary</span>
-                    <div className="Set">
-                        <Marker rotation={this.state.complementaryMarker} colorScheme="Complementary"/>
-                        <Wheel selection={this.state.selectedColorScheme === "Complementary" ? "SelectedWheel" : ""} onClick={(color) => this.handleClick(color, "Complementary")}/>
+                    <div className="Left-column">
+                        <div className="Set">
+                            <span
+                                className={"Label " + (this.state.selectedColorScheme === "Complementary" ? "Selected" : "")}>Complementary</span>
+                            <div className="Wheel-container">
+                                <Marker rotation={this.state.complementaryMarker} colorScheme="Complementary"/>
+                                <Wheel
+                                    selection={this.state.selectedColorScheme === "Complementary" ? "SelectedWheel" : ""}
+                                    onClick={(color) => this.handleClick(color, "Complementary")}/>
+                            </div>
+                        </div>
+                        <div className="Set">
+                            <span
+                                className={"Label " + (this.state.selectedColorScheme === "Analogous" ? "Selected" : "")}>Analogous</span>
+                            <div className="Wheel-container">
+                                <Marker rotation={this.state.analogousMarker} colorScheme="Analogous"/>
+                                <Wheel selection={this.state.selectedColorScheme === "Analogous" ? "SelectedWheel" : ""}
+                                       onClick={(color) => this.handleClick(color, "Analogous")}/>
+                            </div>
+                        </div>
+                        <div className="Set">
+                            <span
+                                className={"Label " + (this.state.selectedColorScheme === "SplitComplementary" ? "Selected" : "")}>Split complementary</span>
+                            <div className="Wheel-container">
+                                <Marker rotation={this.state.splitComplementaryMarker}
+                                        colorScheme="SplitComplementary"/>
+                                <Wheel
+                                    selection={this.state.selectedColorScheme === "SplitComplementary" ? "SelectedWheel" : ""}
+                                    onClick={(color) => this.handleClick(color, "SplitComplementary")}/>
+                            </div>
+                        </div>
                     </div>
-                    <span className={"Label " + (this.state.selectedColorScheme === "Analogous" ? "Selected" : "")}>Analogous</span>
-                    <div className="Set">
-                        <Marker rotation={this.state.analogousMarker} colorScheme="Analogous"/>
-                        <Wheel selection={this.state.selectedColorScheme === "Analogous" ? "SelectedWheel" : ""} onClick={(color) => this.handleClick(color, "Analogous")}/>
+                    <div className="Middle">
+                    <div className="Robot">
+                        <Robot
+                            shapeColor1={this.state.shapeColor1}
+                            shapeColor2={this.state.shapeColor2}
+                            shapeColor3={this.state.shapeColor3}
+                            shapeColor4={this.state.shapeColor4}
+                            shapeColor5={this.state.shapeColor5}
+                            shapeColor6={this.state.shapeColor6}
+                        />
+                        <div className={"Text"}>{this.state.text}</div>
                     </div>
-                    <span className={"Label " + (this.state.selectedColorScheme === "SplitComplementary" ? "Selected" : "")}>Split complementary</span>
-                    <div className="Set">
-                        <Marker rotation={this.state.splitComplementaryMarker} colorScheme="SplitComplementary"/>
-                        <Wheel selection={this.state.selectedColorScheme === "SplitComplementary" ? "SelectedWheel" : ""} onClick={(color) => this.handleClick(color, "SplitComplementary")}/>
                     </div>
-                </div>
-                <div className="Robot">
-                <Robot
-                    shapeColor1={this.state.shapeColor1}
-                    shapeColor2={this.state.shapeColor2}
-                    shapeColor3={this.state.shapeColor3}
-                    shapeColor4={this.state.shapeColor4}
-                    shapeColor5={this.state.shapeColor5}
-                    shapeColor6={this.state.shapeColor6}
-                />
-                <span className={"Text"}>{this.state.text}</span>
-                </div>
-                <div>
-                <span className={"Label " + (this.state.selectedColorScheme === "Triadic" ? "Selected" : "")}>Triadic</span>
-                <div className="Set">
-                    <Marker rotation={this.state.triadicMarker} colorScheme="Triadic"/>
-                    <Wheel selection={this.state.selectedColorScheme === "Triadic" ? "SelectedWheel" : ""} onClick={(color) => this.handleClick(color, "Triadic")}/>
-                </div>
-                <span className={"Label " + (this.state.selectedColorScheme === "Tetradic" ? "Selected" : "")}>Tetradic (rectangular)</span>
-                <div className="Set">
-                    <Marker rotation={this.state.tetradicMarker} colorScheme="Tetradic"/>
-                    <Wheel selection={this.state.selectedColorScheme === "Tetradic" ? "SelectedWheel" : ""} onClick={(color) => this.handleClick(color, "Tetradic")}/>
-                </div>
-                <span className={"Label " + (this.state.selectedColorScheme === "Square" ? "Selected" : "")}>Square</span>
-                <div className="Set">
-                    <Marker rotation={this.state.squareMarker} colorScheme="Square"/>
-                    <Wheel selection={this.state.selectedColorScheme === "Square" ? "SelectedWheel" : ""} onClick={(color) => this.handleClick(color, "Square")}/>
-                </div>
-                </div>
+                    <div className="Right-column">
+                        <div className="Set">
+                            <span
+                                className={"Label " + (this.state.selectedColorScheme === "Triadic" ? "Selected" : "")}>Triadic</span>
+                            <div className="Wheel-container">
+                                <Marker rotation={this.state.triadicMarker} colorScheme="Triadic"/>
+                                <Wheel selection={this.state.selectedColorScheme === "Triadic" ? "SelectedWheel" : ""}
+                                       onClick={(color) => this.handleClick(color, "Triadic")}/>
+                            </div>
+                        </div>
+                        <div className="Set">
+                            <span
+                                className={"Label " + (this.state.selectedColorScheme === "Tetradic" ? "Selected" : "")}>Tetradic (rectangular)</span>
+                            <div className="Wheel-container">
+                                <Marker rotation={this.state.tetradicMarker} colorScheme="Tetradic"/>
+                                <Wheel selection={this.state.selectedColorScheme === "Tetradic" ? "SelectedWheel" : ""}
+                                       onClick={(color) => this.handleClick(color, "Tetradic")}/>
+                            </div>
+                        </div>
+                        <div className="Set">
+                            <span
+                                className={"Label " + (this.state.selectedColorScheme === "Square" ? "Selected" : "")}>Square</span>
+                            <div className="Wheel-container">
+                                <Marker rotation={this.state.squareMarker} colorScheme="Square"/>
+                                <Wheel selection={this.state.selectedColorScheme === "Square" ? "SelectedWheel" : ""}
+                                       onClick={(color) => this.handleClick(color, "Square")}/>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
