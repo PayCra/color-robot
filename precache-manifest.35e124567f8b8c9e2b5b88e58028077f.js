4d1ff71e84a72cbefdbfebde88425b986dc1c7ae
paula <paula.krupa@gmail.com>
2020-10-13 20:16:40 +0200

    Updates

--- precache-manifest.35e124567f8b8c9e2b5b88e58028077f.js
+++ precache-manifest.35e124567f8b8c9e2b5b88e58028077f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b109b8a1390d0e215fdcd6e8f012691b",
+    "revision": "8218011e0df1636fabc7e620068c0f30",
     "url": "/color-robot/index.html"
   },
   {
-    "revision": "0ba6cfd6b35664033762",
-    "url": "/color-robot/static/css/main.2cd816b4.chunk.css"
+    "revision": "9bfeefac69dcc776fef6",
+    "url": "/color-robot/static/css/main.7555eb1c.chunk.css"
   },
   {
     "revision": "934f1019d33102134c6e",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/color-robot/static/js/2.92950f3a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0ba6cfd6b35664033762",
-    "url": "/color-robot/static/js/main.915690c7.chunk.js"
+    "revision": "9bfeefac69dcc776fef6",
+    "url": "/color-robot/static/js/main.1a0cd2e7.chunk.js"
   },
   {
     "revision": "5ae955e6fdabb54cc03b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/color-robot/precache-manifest.d26ead21af909237de8cda6274174a8f.js"
+  "/color-robot/precache-manifest.35e124567f8b8c9e2b5b88e58028077f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/color-robot/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
